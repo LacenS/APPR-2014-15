7e4a69bb23ff5b0fbcf2033475b35d37329dc94d
Spela Lacen <spela.lacen@gmail.com>
2015-01-13 00:03:10 +0100

    Uvoz dodatnih tabel
    Uvozila sem se dodatne tabele, da bom lahko kasneje primerjala povezavo
    med delezem uporabnikov interneta ter razvitostjo posameznih drzav

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -17,33 +17,94 @@ uvoz_tabele1 <- function() {
 
 
 
-#uvoz podatkov iz spleta
+#uvoz podatkov iz spleta iz te strani: http://www.internetlivestats.com/internet-users-by-country/
+#tabela prikazuje podatke o stevilu uporabnikov interneta po posameznih drzavah v letu 2014, delez populacije itd...
 library(XML)
 uvoz_tabele2<-function(){
-  u<-"http://www.internetlivestats.com/internet-users-by-country/" #Tabela prikazuje podatke o letu 2014
+  u<-"http://www.internetlivestats.com/internet-users-by-country/"
   tables <- readHTMLTable(u)
   n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
   
-  tabela2 <- tables[[which.max(n.rows)]]
+  r <- tables[[which.max(n.rows)]]
   stolpci <- gsub("\\s+", " ", colnames(r))
+  stolpci <- c(stolpci[2], stolpci[1], stolpci[3:length(stolpci)])
+  rownames(r)<-r[,1]
   
-  tabela2 <- data.frame(Country = tabela2$Country, tabela2["Rank"],
-                  apply(tabela2[3:10], 2,
+  r <- data.frame(r["Country"], r["Rank"],
+                  apply(r[3:10], 2,
                         function(x) as.numeric(gsub("[,%]", "", x))))
-  colnames(tabela2) <- stolpci[-2]
-  return(tabela2)
+  colnames(r) <- stolpci
+  return(r)
 }
+
 
 #Tabela prikazuje uporabnike interneta svetovnega prebivalstva med leti 1993-2014
 uvoz_tabele3<-function(){
   u<-"http://www.internetlivestats.com/internet-users/#trend" 
   tables <- readHTMLTable(u)
-  tabela <- tables[2] # tabela, ki jo zelim je drugi element na tej spletni strani
-  stolpci <- c('Year', 'Internet users', 'Users growth', 'World population', 
-               'Population growth', 'Penetration(% of population with Internet)')
+  tabela3 <- tables[2] # tabela, ki jo zelim je drugi element na tej spletni strani
+  stolpci <- c('Year', 'Internet users', 'Users growth %', 'World population', 
+               'Population growth %', 'Penetration (% of population with Internet)')
+  tabela3 <- data.frame(tabela3)
+  colnames(tabela3)<-stolpci
+  tabela3 <- data.frame(tabela3["Year"],
+                        apply(tabela3[2:6], 2,
+                              function(x) as.numeric(gsub("[,%]", "", x))))
+  return(tabela3[1:22, ])
   
   
 }
 
+#Uvoz tabele iz te strani: http://www.internetworldstats.com/stats.htm
+#Tabela prikazuje uporabnike interneta po geografskih regijah
+uvoz_tabele4<-function(){
+  u<-"http://www.internetworldstats.com/stats.htm" 
+  tables <- readHTMLTable(u)
+  t4 <- data.frame(tables[6])
+  stolpci <- c('World Regions', 'Population(2014 Est.)', 
+               'Internet Users\nDec. 31, 2000', 'Internet Users\nLatest Data', 
+               'Penetration(% Population)', 'Growth\n2000-2014', 'Users %\nof Table')
+  colnames(t4) <- stolpci
+  t4 <- t4[2:9,]
+  rownames(t4) <- t4[,1]
+  t4 <- t4[, 2:7]
+  vrstice <- rownames(t4)
+  stolpci <- colnames(t4)
+  t4 <- data.frame(Country=vrstice, apply(tab[1:6], 2 ,function(x) as.numeric(gsub("[,%]", "",x))))
+  
+  colnames(t4) <- c("Country", stolpci)
+  return(t4)
+  
+  
+  
+}
+
+# Uvoz 5 tabele, ki prikazuje drzave glede na "Income group""(za primerjavo med delezem uporabnikov interneta)
+# v obliki .csv iz te strani: http://databank.worldbank.org/data/views/reports/tableview.aspx?isshared=true
+uvoz_tabele5 <- function() {
+  t5 <- read.csv("gdp.pc.csv")
+  t5 <- t5[,c(1,4)]
+  t5 <- data.frame( Country=t5$X...Country.Name, apply(t5[2], 2 ,function(x) gsub(": nonOECD", "", x)))
+  t5 <- data.frame( Country=t5["Country"], apply(t5[2], 2 ,function(x) gsub(": OECD", "", x)))
+  leveli <- c("Low income", "Lower middle income", "Upper middle income",  "High income")
+  t5$IncomeGroup<-factor(t5$IncomeGroup, levels=leveli, ordered=TRUE)
+  return(t5)
+  
+}
+
+# Uvoz 6 tabele, ki prikazuje gdp pc
+uvoz_tabele6 <- function() {
+  t6 <- read.csv("gdp.pc.st.csv", skip=1)
+  t6 <- t6[, c(1, 45 :58)] # da imam podatke samo od 200-2013 (2014 so tako samo "NA-ji")
+  return(t6) #ne cisto "urejena tabela" 
+  
+  
+}
+
+  
+
+
 cat("Uvazam podatke o uporabnikih interneta...\n")
+
+
 
