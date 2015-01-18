d757b2bfb437838c15b5d3beb44533d3bfeb64e3
Spela Lacen <spela.lacen@gmail.com>
2015-01-18 03:12:35 +0100

    Dodan se en zemljevid
    Ni se cisto dokoncan(naslovi itd...) pa zgleda, da ni cist ok
    pobarvan(podatki v tabeli se zdijo drugac ok)

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -13,25 +13,7 @@ svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthd
                         "svet", "ne_50m_admin_0_countries.shp", mapa = "zemljevid",
                         encoding = "Windows-1252")
 
-
-
-# preuredi <- function(podatki, zemljevid) {
-#   nove.drzave <- svet$name_long[!svet$name_long %in% row.names(podatki)]
-#   manjkajo <- ! nove.drzave %in% rownames(podatki)
-#   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-#   names(M) <- names(podatki)
-#   row.names(M) <- nove.drzave[manjkajo]
-#   podatki <- rbind(podatki, M)
-#   
-#   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$name_long)[rank(zemljevid$name_long)]), ]
-#   if (ncol(podatki) == 1) {
-#     out <- data.frame(out)
-#     names(out) <- names(podatki)
-#     rownames(out) <- rownames(podatki)
-#   }
-#   return(out)
-# }
-
+#preurejanje tabel, ker funkcija preuredi ni delovala
 imena.svet <- gsub("Saint", "St.",
                    iconv(svet$name_long, from = "UTF-8", to = "ASCII//TRANSLIT"))
 names(imena.svet) <- imena.svet
@@ -61,26 +43,40 @@ imena.t2["Sao Tome and Principe"] <- grep("Principe", svet$name_long, value=TRUE
 imena.t2["South Korea"] <- "Republic of Korea"
 imena.t2["Viet Nam"] <- "Vietnam"
 
+# razen manjkajocih se imena ujemajo
+imena.t8 <- rownames(t8)
+names(imena.t8) <- imena.t8
+
+
 m1 <- match(imena.svet, rownames(t1))
 m2 <- match(as.character(svet$name_long), imena.t2)
+m3 <- match(as.character(svet$name_long), imena.t8)
 tab1 <- data.frame(t1[m1,])
 tab2 <- data.frame(t2[m2,])
+tab3 <- data.frame(t8[m3,])
+
 svet$X2000 <-tab1$X2000
 svet$X2007 <-tab1$X2007
-
+svet$X2011 <-tab1$X2011
 svet$X2014 <- tab2$Penetration....of.Pop..with.Internet.
+svet$leta <- as.numeric(tab3$t8.m3...)
 
 
 # vektor barv
 #ramp <- colorRamp(c("blue", "white"))
 #vektor <- rgb( ramp(seq(0, 1, length = 10)), max = 255)
 vektor <- c(brewer.pal(9, "Blues"),"black")
+vektor2 <- c(brewer.pal(9, "BuPu"),"#330033")
 barve <- ifelse(is.na(svet$X2000), "white", "black")
 barve <- vektor[floor(svet$X2000/10) + 1]
 barve2 <- ifelse(is.na(svet$X2007), "white", "black")
 barve2 <- vektor[floor(svet$X2007/10) + 1]
 barve3 <- ifelse(is.na(svet$X2014), "white", "black")
 barve3 <- vektor[floor(svet$X2014/10) + 1]
+barve11 <- ifelse(is.na(svet$X2011), "white", "black")
+barve11 <- vektor[floor(svet$X2011/10) + 1]
+barve_leta <- ifelse(is.na(svet$leta), "white", "black")
+barve_leta <- vektor2[floor(svet$leta/10) + 1]
 kategorije <- c("0-10 %", "10-20 %", "20-30 %", "30-40 %", "40-50 %", 
                 "50-60 %", "60-70 %", "70-80 %", "80-90 %", "90-100 %")
 
@@ -110,13 +106,47 @@ dev.off()
 # na zemljevidu za 2014 bom oznacila drzave, ki spadajo v high income group
 cat("Rišem zemljevid deleza uporabnikov interneta po svetu v letu 2014 z oznacenimi drzavami, ki spadajo v \"high income group\". \n")
 pdf("slike/zemljevid2.pdf", width=6, height=4)
+par(mar = rep(2, 4))
 print(plot(svet, col=barve3))
 title("Uporabniki interneta v letu 2014 \n z oznacenimi drzavami, ki spadajo v \"High income group\"", cex.main = 1,   font.main= 2, col.main= "black")
-drzave1 <- which(svet$income_grp=="1. High income: OECD")
+drzave1 <- which(svet$income_grp=="1. High income: OECD" | svet$income_grp=="2. High income: nonOECD"  )
 legend("bottom",kategorije, fill = vektor,
        border = "black", cex=.392, xjust=0.5)
 points(coordinates(svet[drzave1,]), pch = 20, col="White", cex=.3) #vidimo da se ujema z drzavami z najvecjim delezem..(oznake bom se kasneje spremenila..)
+print(plot(svet, col=barve3))
+title("Uporabniki interneta v letu 2014 \n z oznacenimi drzavami, ki spadajo v \"Low income group\"", cex.main = 1,   font.main= 2, col.main= "black")
+drzave2 <- which(svet$income_grp=="5. Low income" )
+legend("bottom",kategorije, fill = vektor,
+       border = "black", cex=.392, xjust=0.5)
+points(coordinates(svet[drzave2,]), pch = 20, col="Red", cex=.3) #vidimo da se ujema z drzavami z najvecjim delezem..(oznake bom se kasneje spremenila..)
+
 dev.off()
+
+# 3 zemljevid primerjava pricakovane zivljenjske dobe in deleza uporabnikov v letu 2011
+cat("Rišem zemljevid deleza uporabnikov interneta po svetu v letu 2011 v primerjavi s pricakovano zivljenjsko dobo. \n")
+pdf("slike/zemljevid3.pdf", width=6, height=4)
+par(mar = rep(2, 4))
+print(plot(svet, col=barve11)) 
+legend("bottom",kategorije, fill = vektor,
+       border = "black", cex=.42, xjust=0.5, horiz=TRUE)
+title("Uporabniki interneta v letu 2011", 
+      cex.main = 2,   font.main= 3, col.main= "black")
+print(plot(svet, col=barve_leta))
+title("Pricakovana zivljenjska doba v letu 2011", 
+      cex.main = 2,   font.main= 3, col.main= "black")
+legend("bottom",kategorije, fill = vektor2,
+       border = "black", cex=.42, xjust=0.5, horiz=TRUE)
+
+dev.off()
+
+
+
+
+# spplot(svet, c("X2011", "SID79"), names.attr = c("1974","1979"),
+#        colorkey=list(space="bottom"), scales = list(draw = TRUE),
+#        main = "SIDS (sudden infant death syndrome) in North Carolina",
+#        sp.layout = list(arrow), as.table = TRUE)
+
 
 
 
@@ -129,9 +159,24 @@ dev.off()
 
 
 
-#zemljevid bo glede na leto 2014 + kategorije zivljenjske dobe(ce ne bo zgledalo ok pa bom naredila graf)
 
 
+# preuredi <- function(podatki, zemljevid) {
+#   nove.drzave <- svet$name_long[!svet$name_long %in% row.names(podatki)]
+#   manjkajo <- ! nove.drzave %in% rownames(podatki)
+#   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
+#   names(M) <- names(podatki)
+#   row.names(M) <- nove.drzave[manjkajo]
+#   podatki <- rbind(podatki, M)
+#   
+#   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$name_long)[rank(zemljevid$name_long)]), ]
+#   if (ncol(podatki) == 1) {
+#     out <- data.frame(out)
+#     names(out) <- names(podatki)
+#     rownames(out) <- rownames(podatki)
+#   }
+#   return(out)
+# }
 
 
 
